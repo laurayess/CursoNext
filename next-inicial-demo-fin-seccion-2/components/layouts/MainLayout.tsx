0217ada1511fdc6b68bd979cbebf0db6aa5a53c8
laurayess <laura.yessenia.sanchez@gmail.com>
2023-04-13 22:47:11 -0500

    inicio

---
+++ next-inicial-demo-fin-seccion-2/components/layouts/MainLayout.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import { Navbar } from '../Navbar';
+
+
+import styles from './MainLayout.module.css';
+import { FC } from 'react';
+
+export const MainLayout: FC = ({ children }) => {
+  return (
+    <div className={styles.container}>
+
+      <Head>
+        <title>Home - Fernando</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>
+
+        {children}
+
+      </main>
+    </div>
+  )
+};
